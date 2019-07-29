673c8d2dc9a476ec87297318859edddeda7c2c41
zfu61 <zfu61@gatech.edu>
2019-07-29 15:23:36 -0400

    Yet another commit

---
+++ Importer.py
@@ -0,0 +1,6 @@
+# survey link
+# https://forms.office.com/Pages/ResponsePage.aspx?id=u5ghSHuuJUuLem1_Mvqggzn7pMeqnt5NvDoyGZNrY95UMVBYNUhHUDNWSlNHVjVPTko4VDU3WjRLMS4u
+
+import openpyxl
+
+# open xlsx
--- Worker.py
+++ Worker.py
@@ -1,9 +1,12 @@
-class worker(object):
-	def __init__(self, name, availablity):
+class Worker(object):
+	def __init__(self, name, availability, isSupervisor):
 		self.name = name
-		self.aval2am = availablity[0]
-		self.aval2pm = availablity[1]
-		self.aval3am = availablity[2]
-		self.aval3pm = availablity[3]
-		self.aval4am = availablity[4]
-		self.aval4pm = availablity[5]
+		self.aval2am = availability[0]
+		self.aval2pm = availability[1]
+		self.aval3am = availability[2]
+		self.aval3pm = availability[3]
+		self.aval4am = availability[4]
+		self.aval4pm = availability[5]
+
+		self.score = 0
+		self.isSupervisor = isSupervisor
